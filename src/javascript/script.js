a6ee3c24bd2a1baa93f526c3a786e76d27ba5149
nahidhk <nahidappltd@gmail.com>
2025-03-03 00:25:32 +0600

    new update

---
+++ src/javascript/script.js
@@ -0,0 +1,9 @@
+console.log("Hello, world!");
+
+
+function callTheopt(){
+  const callWiki =  confirm("The Ariticle post is a Github page Wiki page , comment on the page to get the link to the page"); 
+if(callWiki === true){
+    window.open("https://github.com/nahidhk/icx/wiki");
+}
+}
