aefd4c14685db65000d280464b477104b5c47a90
nahidhk <nahidappltd@gmail.com>
2025-04-03 12:09:10 +0600

    update

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -3,10 +3,7 @@ console.log("Hello, world!");
 
 
 function callTheopt() {
-  const callWiki = confirm("The Ariticle post is a Github page Wiki page , comment on the page to get the link to the page");
-  if (callWiki === true) {
-    window.open("https://github.com/nahidhk/icx/wiki");
-  }
+  alert("asiya ")
 }
 
 
